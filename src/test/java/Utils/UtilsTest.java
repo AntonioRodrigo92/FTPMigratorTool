0e6007115c49a01ee3de5454cacc10efa9b9253e
Antonio <antoniorodrigo92@gmail.com>
2022-01-15 12:03:29 +0000

    Testes do Utils

--- src/test/java/Utils/UtilsTest.java
+++ src/test/java/Utils/UtilsTest.java
@@ -1,10 +1,14 @@
 package Utils;
 
+import TaskHandler.RunnableTask;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
+import java.time.Instant;
 import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.Calendar;
 import java.util.Date;
 
@@ -66,25 +70,78 @@ class UtilsTest {
 
     @Test
     void sumOneDaySuccess() {
+        LocalDate dayTest = LocalDate.of(2022, 1, 15);
+        LocalDate dayAfterTest = LocalDate.of(2022, 1, 16);
+        LocalDate dayAfter = Utils.sumOneDay(dayTest);
+        Assertions.assertEquals(dayAfter, dayAfterTest);
     }
 
     @Test
     void sumOneDayFailure() {
+        LocalDate dayTest = LocalDate.of(2022, 1, 15);
+        LocalDate dayAfter = Utils.sumOneDay(dayTest);
+        Assertions.assertNotEquals(dayAfter, dayTest);
     }
 
     @Test
     void getCurrentDateTimeSuccess() {
+        Date currentTest = new Date(System.currentTimeMillis());
+        Date current = Utils.getCurrentDateTime();
+
+        int dayTest = currentTest.getDate();
+        int day = current.getDate();
+        int monthTest = currentTest.getMonth();
+        int month = current.getMonth();
+        int yearTest = currentTest.getYear();
+        int year = current.getYear();
+        int hourTest = currentTest.getHours();
+        int hour = current.getHours();
+        int minutesTest = currentTest.getMinutes();
+        int minutes = current.getMinutes();
+        int secondsTest = currentTest.getSeconds();
+        int seconds = current.getSeconds();
+
+        Assertions.assertEquals(day, dayTest);
+        Assertions.assertEquals(month, monthTest);
+        Assertions.assertEquals(year, yearTest);
+        Assertions.assertEquals(hour, hourTest);
+        Assertions.assertEquals(minutes, minutesTest);
+        Assertions.assertEquals(seconds, secondsTest);
+    }
+
+    @Test
+    void getCurrentDateTimeSuccess2() {
+        Date currentTest = new Date(System.currentTimeMillis());
+        Date current = Utils.getCurrentDateTime();
+
+        Instant instantTest = currentTest.toInstant().truncatedTo(ChronoUnit.SECONDS);
+        Instant instant = current.toInstant().truncatedTo(ChronoUnit.SECONDS);
+
+        Assertions.assertEquals(instant, instantTest);
     }
 
     @Test
     void getCurrentDateTimeFailure() {
+        Date currentTest = new Date();
+        Calendar calTest = Calendar.getInstance();
+        calTest.setTime(currentTest);
+        calTest.add(Calendar.DATE, 1);
+        currentTest = calTest.getTime();
+        Date current = Utils.getCurrentDateTime();
+
+        Instant instantTest = currentTest.toInstant().truncatedTo(ChronoUnit.SECONDS);
+        Instant instant = current.toInstant().truncatedTo(ChronoUnit.SECONDS);
+
+        Assertions.assertNotEquals(instant, instantTest);
     }
 
     @Test
-    void docToTunnableTaskSuccess() {
+    void docToRunnableTaskSuccess() {
+
     }
 
     @Test
-    void docToTunnableTaskFailure() {
+    void docToRunnableTaskFailure() {
+
     }
 }
